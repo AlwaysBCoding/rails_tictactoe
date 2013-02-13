34442dbea269a25d9a0ba8b46bedc4e4422e0691
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-02-13 11:29:04 -0600

    Iniital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JdlTictactoe::Application.config.secret_token = 'f4b19b2e9d2b13438cce82a100eb78b55e2fb691a080290ce33ad347e4944fb49daddc094d37b77585141e93e729eb213d0551a46f677adaffb5d71a06c2ae10'
